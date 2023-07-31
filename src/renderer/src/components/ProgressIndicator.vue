42abb1c5f1c912e41a9c8308f239169cf05c4467
David Ferguson <david@ferg.co>
2023-07-30 21:41:42 -0500

    hide progress bar on reset

--- src/renderer/src/components/ProgressIndicator.vue
+++ src/renderer/src/components/ProgressIndicator.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { useProgress } from '@/composition/useProgress'
-const { percent } = useProgress()
+const { percent, show } = useProgress()
 </script>
 
 <template>
-    <div class="absolute top-6 left-0 w-full h-0.5 z-20">
+    <div class="absolute top-6 left-0 w-full h-0.5 z-20" v-if="show">
         <div class="progress-step h-full bg-blue-400" :style="{ width: percent + '%' }"></div>
     </div>
 </template>
--- src/renderer/src/composition/useProgress.ts
+++ src/renderer/src/composition/useProgress.ts
@@ -8,6 +8,7 @@ const percent = computed(() => {
   if (val <= 100) return val * 100
   else return 100
 })
+const show = ref(false)
 
 export const useProgress = () => {
   function setTotal(to: number) {
@@ -15,27 +16,32 @@ export const useProgress = () => {
   }
 
   function set(to: number) {
+    show.value = true
     complete.value = to
 
     if (percent.value >= 100) setTimeout(() => reset(), 1000)
   }
 
   function increment(by: number = 1) {
+    show.value = true
     complete.value += by
 
     if (percent.value >= 100) setTimeout(() => reset(), 1000)
   }
 
   function decrement(by: number = 1) {
+    show.value = true
     complete.value -= by
   }
 
   function reset() {
     total.value = 0
     complete.value = 0
+    show.value = false
   }
 
   return {
+    show,
     setTotal,
     set,
     increment,
